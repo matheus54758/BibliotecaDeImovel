d4811d23e33212105a7ed2af813ca7c4065994fc
matheus54758 <matheus54759@gmail.com>
2026-05-02 14:00:09 -0300

    Fix: revert base path to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/Lumis/',
+    base: '/BibliotecaDeImovel/',
     plugins: [react()],
     define: {
       'process.env': env
